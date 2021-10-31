d55de6848f7308c60fe441017a2ce3c2dbb0b0b6
Abhee Hudani <62197910+Novartus@users.noreply.github.com>
2021-10-31 10:01:09 +0530

    Feature : RSS-Reader added, tsconfig compiler target changed from es5 to es6

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+export default function App() {
+  const [rssUrl, setRssUrl] = useState("");
+  const [items, setItems] = useState([{ title: "", link: "", author: "" }]);
+  const getRss = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const urlRegex =
+      /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
+    if (!urlRegex.test(rssUrl)) {
+      return;
+    }
+    const res = await fetch(`https://api.allorigins.win/get?url=${rssUrl}`);
+    const { contents } = await res.json();
+    const feed = new window.DOMParser().parseFromString(contents, "text/xml");
+    const items = feed.querySelectorAll("item");
+    const feedItems = [...items].map((el: any) => ({
+      link: el.querySelector("link").innerHTML,
+      title: el.querySelector("title").innerHTML,
+      author: el.querySelector("author").innerHTML,
+    }));
+    setItems(feedItems);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getRss}>
+        <div>
+          <label> rss url</label>
+          <br />
+          <input onChange={(e) => setRssUrl(e.target.value)} value={rssUrl} />
+        </div>
+        <input type="submit" />
+      </form>
+      {items.map((item: any) => {
+        return (
+          <div>
+            <h1>{item.title}</h1>
+            <p>{item.author}</p>
+            <a href={item.link}>{item.link}</a>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
